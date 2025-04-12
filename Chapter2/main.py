9e0c8819fda7f5c54c59d8c132e6201cc5a49c10
noway-code <dragonfury1928@gmail.com>
2025-04-11 20:34:50 -0400

    Data cleaning

--- Chapter2/main.py
+++ Chapter2/main.py
@@ -1,7 +1,25 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from pydoc import describe
+
+import pandas as pd
+
+
+def main():
+    # index_col indicates the first col (college names) are just labels
+    college = pd.read_csv("../datasets/College.csv")
+    college2 = pd.read_csv("../datasets/College.csv", index_col=0)
+
+    college3 = college.rename({'Unnamed: 0': 'College'}, axis=1)
+    college3 = college3.set_index('College')
+    college = college3
+    print(college.head(10))
+    print(college.describe())
+
 
 # Function only called when ran directly, not on import
 if __name__ == '__main__':
-    print_hi('Michael')
-
+    bold_start = "\033[1m"
+    bold_end = "\033[0m"
+    print(bold_start + "\nRunning Chapter 2" + bold_end)
+    print(
+        bold_start + "====================================================================================================\n\n" + bold_end)
+    main()
